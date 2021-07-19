5c2a3244af7229b81936a83cb8510560bb63bcaa
Jinwoo Kang <jikang@student.42seoul.kr>
2021-07-19 16:05:38 +0900

    Chore: 프로젝트 환경 설정
    프로젝트 진행을 위한 환경 설정
    
    Resolves: #2

---
+++ commitlint.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': () => [2, 'always', 50],
+    'type-case': [2, 'always', 'pascal-case'],
+    'type-enum': [2, 'always', ['Chore', 'Docs', 'Feat', 'Fix', 'Refactor', 'Style', 'Test']],
+  },
+};
