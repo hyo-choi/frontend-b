088e8495e5fccb98d6d2472925ba85ed2ad7d42c
Jinwoo Kang <jikang@student.42seoul.kr>
2021-07-20 20:39:23 +0900

    Feat: Avatar component 제작
    메인페이지의 Avatar 제작
    
    Resolves: #6

---
+++ src/components/atoms/Avatar/Avatar.stories.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable arrow-body-style */
+import React from 'react';
+import { Meta } from '@storybook/react';
+import Avatar from './Avatar';
+
+export default {
+  component: Avatar,
+  title: 'atoms/Avatar',
+} as Meta;
+
+export const Avatars = () => {
+  return (
+    <>
+      <Avatar alt="Name" />
+      <Avatar
+        alt="jikang"
+        src="https://cdn.intra.42.fr/users/medium_jikang.jpg"
+      />
+      <Avatar
+        alt="Travis"
+        src="https://material-ui.com/static/images/avatar/2.jpg"
+      />
+    </>
+  );
+};
