7469462224cf05846bcab7576b8ac54c37210ade
Jinwoo Kang <jikang@student.42seoul.kr>
2021-07-20 14:19:39 +0900

    Fix: commitlint subject-case rule 수정
    commitlint subject-case rule 수정
    
    Resolves: #11

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,5 +4,6 @@ module.exports = {
     'header-max-length': () => [2, 'always', 50],
     'type-case': [2, 'always', 'pascal-case'],
     'type-enum': [2, 'always', ['Chore', 'Docs', 'Feat', 'Fix', 'Refactor', 'Style', 'Test']],
+    'subject-case': [0, 'always', 0],
   },
 };
